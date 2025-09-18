5709b087d420639c55182a351b3929af8a0406ad
nellex <daniil14227@yandex.ru>
2025-09-18 16:44:49 +0300

    test auth with sdk 3 version

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,23 @@
 import { createRoot } from "react-dom/client";
+import { retrieveLaunchParams, init as initSDK } from '@telegram-apps/sdk-react';
+import '@telegram-apps/telegram-ui/dist/styles.css';
 import App from "./App.tsx";
 import "./index.css";
-import { init } from '@telegram-apps/sdk-react';
 
-init();
+(async () => {
+  try {
+    const launchParams = retrieveLaunchParams();
+    const debug = import.meta.env.DEV;
 
-createRoot(document.getElementById("root")!).render(<App />);
+    await initSDK({
+      debug,
+      eruda: debug,
+      mockForMacOS: launchParams.tgWebAppPlatform === 'macos',
+    });
+
+    createRoot(document.getElementById("root")!).render(<App />);
+  } catch (e) {
+    console.error("Telegram SDK initialization failed:", e);
+    // Здесь можно рендерить fallback для неподдерживаемой среды
+  }
+})();
