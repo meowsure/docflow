1aafdf8234beac9a0fbb892be6efe5d2acf02894
nellex <daniil14227@yandex.ru>
2025-09-21 02:04:53 +0300

    Check auth tg

--- src/components/TelegramAuth.tsx
+++ src/components/TelegramAuth.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { Button } from "@/components/ui/button";
 import { useAuth } from "@/contexts/AuthContext";
-import api from "@/api";
+
 
 const TelegramAuth: React.FC = () => {
   const { user, logout } = useAuth();
 
-  const response = api.get("https://api.marzsure.ru:8444/api/v1/auth/me");
-
-  const text = response.data;
   if (user) {
     return (
       <div className="p-4 space-y-2">
@@ -22,7 +19,6 @@ const TelegramAuth: React.FC = () => {
     <div className="p-4 space-y-2">
       <p>Нет авторизации через Telegram</p>
       <p>Откройте приложение внутри Telegram</p>
-      <code>{text}</code>
     </div>
   );
 };
