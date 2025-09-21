6d1c205604fdeb850e9ca72c1b6666982e1c7e55
nellex <daniil14227@yandex.ru>
2025-09-21 21:54:48 +0300

    Refactor Auth

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -54,7 +54,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
         // Отправляем на сервер
         try {
-          const response = await api.post("/auth/telegram", { initData: mappedUser, auth_date: authDate, });
+          const response = await api.post("/auth/telegram", { user: mappedUser, auth_date: authDate, });
 
           if (response.status === 200 && response.data) {
             // const { token, user } = response.data;
