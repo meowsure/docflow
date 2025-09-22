e062509a177c36cde31508dcf9909eee237812a4
nellex <daniil14227@yandex.ru>
2025-09-22 04:40:00 +0300

    Upload

--- src/hooks/useRoles.ts
+++ src/hooks/useRoles.ts
@@ -4,11 +4,11 @@ import { useCrud } from "./useCrud";
 export interface Role {
   id: string;
   name: string;
-  description: string;
+  description?: string;
   permissions: string[];
   users: { id: string; first_name: string; last_name: string; username: string; photo_url: string }[];
   created_at: string;
-  isSystem: boolean;
+  isSystem?: boolean;
   updated_at: string;
   userCount?: number; // Optional property to hold the number of users in this role
 }
