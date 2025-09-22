750b6e2054a7a92691b2188bea4bfa45c8dc4312
nellex <daniil14227@yandex.ru>
2025-09-22 11:16:51 +0300

    Upload new files

--- src/hooks/useCrud.ts
+++ src/hooks/useCrud.ts
@@ -57,7 +57,7 @@ export const useCrud = <T extends { id: string }>(endpoint: string) => {
       toast({
         variant: "destructive",
         title: "Ошибка",
-        description: "Не удалось создать запись",
+        description: "Не удалось создать запись: "+ err.data ? err.data.message : err.data.message,
       });
       return null;
     }
