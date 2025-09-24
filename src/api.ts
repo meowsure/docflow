7218943ac1cca5a37510e48ba33074cc731cf6ed
nellex <daniil14227@yandex.ru>
2025-09-24 21:02:38 +0300

    Upload new files

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://api.marzsure.ru:8444/api/v1', // заменить на ваш домен
+  baseURL: 'https://api.marzsure.ru:8444/api/v1',
 });
 
 // Добавляем токен к каждому запросу, если он есть
