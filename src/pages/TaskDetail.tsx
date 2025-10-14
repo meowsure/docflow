4492b1d35a05e13c489af66bff2c3269c60acb7c
nellex <daniil14227@yandex.ru>
2025-10-14 13:35:32 +0300

    Upload

--- src/pages/TaskDetail.tsx
+++ src/pages/TaskDetail.tsx
@@ -24,15 +24,14 @@ const TaskDetail = () => {
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-background">
-        <Header />
+
         <div className="container mx-auto px-4 py-8">
           <div className="animate-pulse space-y-4">
             <div className="h-8 bg-muted rounded w-1/3"></div>
             <div className="h-64 bg-muted rounded"></div>
           </div>
         </div>
-      </div>
+
     );
   }
 
